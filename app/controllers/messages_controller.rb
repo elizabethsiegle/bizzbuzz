03ed954cf1bb19101d3319f775cbbb910d8ee675
lsiegle <lsiegle@twilio.com>
2017-08-17 09:27:55 -0400

    send diff response based on body

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,25 +2,30 @@ class MessagesController < ApplicationController
  skip_before_action :verify_authenticity_token
  
   def reply
-    message_body = params["Body"]
-    from_number = params["From"]
+    msg = params["Body"]
+    non_twilio_num = params["From"]
     account_sid = 'AC358a60437a112c5c59d3b52da1f0dcc7'
-    auth_token = 'e7ae1b711f733bae6c2647bd62154b77' #Rails.application.secrets.twilio_token
+    auth_token = 'e7ae1b711f733bae6c2647bd62154b77' 
     @client = Twilio::REST::Client.new account_sid, auth_token
+    if msg == "hi" then
+      body_resp = "yolo"
+    elsif msg == "heyo" then
+      body_resp = "yee"
+    else 
+      body_resp="idk man"
     sms = @client.messages.create(
-    	body: "Hello there, thanks for texting me. Your number is #{from_number}.",
+    	body: body_resp,
     	from: '+12028757342',
-    	to: from_number,
+    	to: non_twilio_num,
     )
-    
+    end
   end
  
   private
  
   def boot_twilio
-    #account_sid = Rails.application.secrets.twilio_sid
     account_sid = 'AC358a60437a112c5c59d3b52da1f0dcc7'
-    auth_token = 'e7ae1b711f733bae6c2647bd62154b77' #Rails.application.secrets.twilio_token
+    auth_token = 'e7ae1b711f733bae6c2647bd62154b77' 
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 end
