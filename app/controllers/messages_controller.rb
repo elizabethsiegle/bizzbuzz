81ae6637e27a027876b377d316ff05e7a040e23e
Jenni Rethi <jennifer.rethi@gmail.com>
2017-08-17 22:01:00 -0500

    added in square api

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,84 @@
 class MessagesController < ApplicationController 
  skip_before_action :verify_authenticity_token
+
+ require 'unirest'
+require 'json'
+require 'ostruct'
+
+class MyClass
+  # Replace this value with your application's personal access token,
+  # available from your application dashboard (https://connect.squareup.com/apps)
+  ACCESS_TOKEN = 'sq0atp-PXtIb9nroEojtUkVGed3KQ'
+
+  # The ID of the location you want to create an item for.
+  # See payments-report.rb for an example of getting a business's location IDs.
+  LOCATION_ID = 'VZXNSMR4PRWT5'
+
+  # The base URL for every Connect API request
+  CONNECT_HOST = 'https://connect.squareup.com'
+
+  # Standard HTTP headers for every Connect API request
+  REQUEST_HEADERS = {
+    'Authorization' => 'Bearer ' + ACCESS_TOKEN,
+    'Accept' => 'application/json',
+    'Content-Type' => 'application/json'
+  }
+                    
+
+  def list_inventory() 
+    response = Unirest.get CONNECT_HOST + '/v1/' + LOCATION_ID + '/items',
+                      headers: REQUEST_HEADERS
+    if response.code == 200
+      #puts 'Successfully created item:'
+      #String responseString = String.new(response.body)
+      json_object = JSON.parse(JSON.pretty_generate(response.body), object_class: OpenStruct)
+      #json_object.inspect
+      puts json_object[0].id
+      return json_object
+    else
+      puts 'location does not exisit'
+      puts response.body
+      return nil
+    end
+  end
+
+
+def get_quantity()
+
+    location_id = list_inventory
+    response = Unirest.get CONNECT_HOST + '/v1/' + LOCATION_ID + '/inventory',
+                      headers: REQUEST_HEADERS
+    if response.code == 200
+      #puts 'Successfully created item:'
+      #String responseString = String.new(response.body)
+      json_object = JSON.parse(JSON.pretty_generate(response.body), object_class: OpenStruct)
+      #json_object.inspect
+      puts json_object[0].quantity_on_hand
+      return json_object[0].quantity_on_hand
+    else
+      puts 'location does not exisit'
+      puts response.body
+      return nil
+    end
+end
  
   def reply
+    json_object = list_inventory
+    quantity_on_hand = get_quantity
+    
     incoming_msg = params["Body"].downcase #convert to lowercase
     external_msg_num = params["From"]
     boot_twilio
-    if incoming_msg == "inventory" then
-      body_resp = "there is x-num left"
-    elsif incoming_msg == "tweet" then
-      body_resp = "post to twitter"
-    else 
-      body_resp = "else lol"
+    if msg == "GET INV" then
+      if quantity_on_hand > json_object[0].variations[0].inventory_alert_threshold then 
+        body_resp = "inventory for" + json_object[0].name + "is low"
+      else 
+        body_resp = "In stock"
+      end
+    elsif msg == "TWEET" then
+      body_resp = "Reply with your tweet message"
+    else
+      body_resp="Invalid SMS Request"
     end
     sms = @client.messages.create(
       :from => '+19094559811',
